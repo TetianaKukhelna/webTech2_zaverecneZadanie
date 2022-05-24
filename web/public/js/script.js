d13813b3a5b77e6afdb9fa4bcf33bb43922faf35
Kubinel <kubinel47@gmail.com>
2022-05-24 21:34:04 +0200

    Added graf

--- web/public/js/script.js
+++ web/public/js/script.js
@@ -1,30 +1,176 @@
+const submitBtn = document.querySelector("#_simulate");
+const output = document.querySelector("#output");
+const api = document.querySelector("#api");
+const name = document.querySelector("#name");
+const displayName = document.querySelector("#displayName");
 
-    const submitBtn = document.querySelector("#_simulate");
-    const output = document.querySelector("#output");
-    const api = document.querySelector("#api");
-    const name = document.querySelector("#name");
-    const displayName = document.querySelector("#displayName");
+api.setAttribute("value", localStorage.getItem("apiKey"));
+name.setAttribute("value", localStorage.getItem("name"));
+displayName.innerHTML = localStorage.getItem("name");
+console.log(localStorage.getItem("name"))
 
-    api.setAttribute("value", localStorage.getItem("apiKey"));
-    name.setAttribute("value", localStorage.getItem("name"));
-    displayName.innerHTML = localStorage.getItem("name");
-    console.log(localStorage.getItem("name"))
+submitBtn.addEventListener('click' , (e) =>{
+    e.preventDefault();
+
+    fetch('http://localhost:8000/server.php', {
+        method:'POST',
+        mode:"cors",
+        body:new FormData(document.querySelector('#sim_form'))
+    }).then(res=>res.json())
+        .then(data =>{
+            output.innerHTML = data.result;
+            console.log(data);
+            parse(data.result);
+        })
+        .catch(err=>console.log(err));
+
+})
+
+let myDataX = [];
+let myDataY1 = [];
+let myDataY2 = [];
+
+function parse(data){
+    result = data;
+    result = result.split("\n");
+
+    var count = 0;
+
+    for (let i = 0; i < result.length; i++) {
+        if(result[i] === ""){
+            count++;
+        }
+        if(count === 3){
+            if (result[i].length > 2){
+                myDataX.push(result[i].trim());
+            }
+        }
+        if(count === 5){
+            if (result[i].length > 2) {
+                let temp;
+                let line;
+                temp = result[i].replace(/  +/g, ' ');
+                line = temp.split(" ");
+
+                myDataY1.push(line[1]);
+
+                myDataY2.push(line[2]);
+            }
+        }
+    }
+    kresli();
+}
+
+function kresli(){
+    let trace1 = {
+        x: myDataX,
+        y: myDataY2,
+        name: 'Dumper',
+        type: 'scatter'
+    };
+
+    var trace2 = {
+        x: myDataX,
+        y: myDataY1,
+        name: 'Car',
+        yaxis: 'x2',
+        type: 'scatter'
+    };
+
+    var data = [trace1, trace2];
+
+    var layout = {
+        yaxis2: {
+            titlefont: {color: 'rgb(148, 103, 189)'},
+            tickfont: {color: 'rgb(148, 103, 189)'},
+            overlaying: 'y',
+            side: 'right'
+        }
+    };
+
+    Plotly.newPlot('graf', data, layout);
+}
+
+//animacia
+let spring;
+let piston1;
+let piston2;
+let canvas;
+
+function start() {
+    canvas = new fabric.Canvas('canvas');
+
+    spring = new fabric.Rect({
+        left: 100,
+        top: 115,
+        fill: 'black',
+        width: 60,
+        height: 5
+    });
+
+    piston1 = new fabric.Rect({
+        left: 120,
+        top: 100,
+        fill: 'blue',
+        width: 20,
+        height: 40
+    });
+
+    piston2 = new fabric.Rect({
+        left: 160,
+        top: 100,
+        fill: 'red',
+        width: 20,
+        height: 40
+    });
+
+    canvas.add(spring);
+    canvas.add(piston1);
+    canvas.add(piston2);
+}
+
+let count = 0;
+function anim(){
+    count++;
+    spring.animate('width', 200, {
+        onChange: canvas.renderAll.bind(canvas),
+        fixedduration: 1000,
+    });
+
+    piston1.animate('left', 180, {
+        onChange: canvas.renderAll.bind(canvas),
+        fixedduration: 1000,
+    });
+
+    piston2.animate('left', 280, {
+        onChange: canvas.renderAll.bind(canvas),
+        fixedduration: 1000,
+    });
+    setTimeout(function(){
+        spring.animate('width', 60, {
+            onChange: canvas.renderAll.bind(canvas),
+            fixedduration: 1000,
+        });
+
+        piston1.animate('left', 120, {
+            onChange: canvas.renderAll.bind(canvas),
+            fixedduration: 1000,
+        });
+
+        piston2.animate('left', 160, {
+            onChange: canvas.renderAll.bind(canvas),
+            fixedduration: 1000,
+        });
+        if(count < 3) {
+            anim();
+        }
+    }, 1000);
 
 
-    submitBtn.addEventListener('click' , (e) =>{
-        e.preventDefault();
+}
 
-        fetch('http://localhost:8000/server.php', {
-            method:'POST',
-            mode:"cors",
-            body:new FormData(document.querySelector('#sim_form'))
-        }).then(res=>res.json())
-            .then(data =>{
-                output.innerHTML = data.result;
-                console.log(data);
-            })
-            .catch(err=>console.log(err));
-
-    })
+function play(){
+    anim();
+}
 
 
--- web/public/simulate.php
+++ web/public/simulate.php
@@ -9,6 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="./js/plotly-2.12.1.min.js"></script>
     <title>Suspension simulation</title>
 </head>
 <body>
@@ -55,6 +56,7 @@
                                 <button id="_navod" name="submit_pdf" class="btn btn-outline-light btn-lg px-5" type="submit">Instruction</button>
                             </form>
                         </div>
+                        <div id="graf"></div>
                     </div>
                 </div>
             </div>
